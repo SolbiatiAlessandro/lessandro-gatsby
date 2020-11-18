4dedbb2ec413d02bfd4b3a649f362c16e259a291
Alessandro Solbiati <lessandro@fb.com>
2020-11-18 22:02:04 +0000

    [external-images] didn't manage to add plugin, changed class

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,30 +42,34 @@ module.exports = {
         ],
       },
     },
+	"gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
 	{
-    resolve: `gatsby-transformer-remark`,
-    options: {
-      plugins: [
-        {
-          resolve: `gatsby-remark-image-attributes`,
-          options: {
-
-            // ?Boolean=true
-            //   If true (the default), all CSS
-            //   property names will be recognized
-            //   as styleAttribute.
-            styleAttributes: true,
-
-            // ?Boolean=false
-            //   If true, all attributes that
-            //   aren't styleAttributes, will be
-            //   added as data-* attributes to the
-            //   image.
-            dataAttributes: false
-			}
-		  }
-		]
-	  }
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-images-anywhere",
+            options: {
+              staticDir: "static",
+              createMarkup: ({
+                src,
+                srcSet,
+                sizes,
+                aspectRatio,
+                alt,
+                base64,
+                presentationWidth,
+              }) => {
+                return `<img test="yea" src="${src}" srcset="${srcSet}" sizes="${sizes}" aspectratio="${aspectRatio}" alt="${alt}" base64="${base64}" presentationwidth="${presentationWidth}"></custom-image>`
+              },
+              sharpMethod: "fluid",
+              // Additional sharp image arguments: https://www.gatsbyjs.org/packages/gatsby-plugin-sharp/
+              // maxWidth: 650,
+            },
+          },
+        ],
+      },
 	},
     {
       resolve: `gatsby-plugin-google-analytics`,
