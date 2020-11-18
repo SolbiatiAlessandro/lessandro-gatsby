c64df6f41079b869a0c9a1d2066b65776aa58ba5
Alessandro Solbiati <lessandro@fb.com>
2020-11-18 22:15:55 +0000

    cleaned external image tentative

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,35 +42,6 @@ module.exports = {
         ],
       },
     },
-	"gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-	{
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [
-          {
-            resolve: "gatsby-remark-images-anywhere",
-            options: {
-              staticDir: "static",
-              createMarkup: ({
-                src,
-                srcSet,
-                sizes,
-                aspectRatio,
-                alt,
-                base64,
-                presentationWidth,
-              }) => {
-                return `<img test="yea" src="${src}" srcset="${srcSet}" sizes="${sizes}" aspectratio="${aspectRatio}" alt="${alt}" base64="${base64}" presentationwidth="${presentationWidth}"></custom-image>`
-              },
-              sharpMethod: "fluid",
-              // Additional sharp image arguments: https://www.gatsbyjs.org/packages/gatsby-plugin-sharp/
-              // maxWidth: 650,
-            },
-          },
-        ],
-      },
-	},
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
